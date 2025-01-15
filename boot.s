e86063940f37bf7f5b12efd285aa1b0afe99a9e7
koredev-lolz <michalciezki1130@gmail.com>
2025-01-15 19:58:30 +0100

    Create boot.s

---
+++ boot.s
@@ -0,0 +1,29 @@
+    .section .multiboot
+    .align 4
+    .equ ALIGN, 1 << 0
+    .equ MEMINFO, 1 << 1
+    .equ FLAGS, ALIGN | MEMINFO
+    .equ MAGIC, 0x1BADB002
+    .equ CHECKSUM, -(MAGIC + FLAGS)
+
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+    .section .bss
+    .align 16
+stack_bottom:
+    .skip 16384
+stack_top:
+
+    .section .text
+    .global _start
+    .type _start,
+_start:
+    mov $stack_top, %esp
+
+    call kernel_main
+
+    cli
+1:  hlt
+    jmp 1b
